c5a9723efc761531ae187c491a05aed8ecf8fbb2
adribarrios05 <abamar0501@g.educaand.es>
2023-10-04 12:48:19 +0200

    4/10/2023

---
+++ CAP01/src/Ej2.java
@@ -0,0 +1,7 @@
+public class Ej2 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Adrián Barrios Márquez");
+        System.out.println("c/Desengaño 21");
+        System.out.print("660487198");
+    }
+}
---
+++ CAP01/src/Ej3.java
@@ -0,0 +1,16 @@
+public class Ej3 {
+    public static void main(String[] args) throws Exception {
+        System.out.printf("%-10s   %6s%n","computer","ordenador");
+        System.out.printf("%-10s    %6s%n","student","alumno");
+        System.out.printf("%-10s    %6s%n","cat","gato");
+        System.out.printf("%-5s    %6s%n","penguin","pinguino");
+        System.out.printf("%-5s    %6s%n","machine","máquina");
+        System.out.printf("%-5s     %6s%n","nature","naturaleza");
+        System.out.printf("%-5s   %6s%n","light","luz");
+        System.out.printf("%-5s     %6s%n","green","verde");
+        System.out.printf("%-5s     %6s%n","book","libro");
+        System.out.printf("%-5s    %6s%n","pyramid","pirámide");
+        
+
+    }
+}
