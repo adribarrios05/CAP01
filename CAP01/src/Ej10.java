cf8292a59f5487c058fe46936cfb14468ffe57c9
adribarrios05 <abamar0501@g.educaand.es>
2023-10-09 08:17:19 +0200

    6/10/2023

--- CAP01/src/Ej10.java
+++ CAP01/src/Ej10.java
@@ -1,5 +1,16 @@
 public class Ej10 {
-    public static void main(String[] args) {
-        System.out.printf("\u2587"+"\u2587"+"\u9623");
-    }
+  public static void main(String[] args) {
+    System.out.println("\033[31m");
+    System.out.println("                        ██");
+    System.out.println("                      ██");
+    System.out.println("                        ██\n\033[34m");
+    System.out.println("                     ████████████");
+    System.out.println("                     ████████  ██");
+    System.out.println("                     ████████████");
+    System.out.println("                     ████████");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░████████░░░░░░░░");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+    System.out.println("\033[37m");
+  }
 }
